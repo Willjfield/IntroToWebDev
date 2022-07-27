7d75f97089b7980fd8e3ecb653ec7001bc902f2d
Willjfield <willjfield@gmail.com>
2022-07-27 09:49:20 -0400

    leaflet

---
+++ 0726/leaflet/main.js
@@ -0,0 +1,32 @@
+var map = L.map('map').setView([42, -72], 13);
+
+let dummyData = [
+    {
+        name: "nyc",
+        lng:-73.96820068359375,
+        lat: 40.772221877329024
+        
+    },
+    {
+        name: "la",
+        lng:-118.125,
+        lat:34.016241889667015
+    }
+]
+
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+let bndsArray = []
+
+dummyData.forEach(function(city){
+    var marker = L.marker([city.lat,city.lng]).addTo(map);
+    bndsArray.push(L.point(city.lat,city.lng))
+});
+
+let bnds = L.bounds(bndsArray);
+map.fitBounds(bnds)
