d315a12249eeca9ac03e9060ef7fde3a3001b668
Willjfield <willjfield@gmail.com>
2022-07-27 16:18:39 -0400

    comments

--- 0726/leaflet/main.js
+++ 0726/leaflet/main.js
@@ -31,29 +31,38 @@ dummyData.forEach(function (city) {
 
 map.fitBounds(bndsArray)
 
-
-
+//Create an empty array that will have all the results appended to
 let allResults = [];
+//Search radius
 let radius = 1;
+//Lat/lng
+let lat = 40.78885994449482;
+let lng = -74.1577148;
 
+//What to do when everything is done
 function processFullResults(){
     //Add all the results to the map
 }
 
+//Take the minimum id to start looking for
 function recursiveGet(min_id, totalResults) {
 
-    let url = `https://api.inaturalist.org/v1/observations?id_above=${min_id}&lat=40.78885994449482&lng=-74.1577148&radius=${radius}&order=asc&order_by=id&per_page=200`
+    //Construct the url
+    let url = `https://api.inaturalist.org/v1/observations?id_above=${min_id}&lat=${lat}&lng=${lng}&radius=${radius}&order=asc&order_by=id&per_page=200`
 
+    //Get the data
     axios.get(url).then((rsp) => {
-
-
+        //Concat the array returned in results to all results
         allResults = allResults.concat(rsp.data.results);
         console.log(allResults)
         console.log(rsp.data.results)
+        //Wait so you don't timeout the API
         setTimeout(() => {
+            //If the length of all the results is still less than the total, increment the minimum id and try again
             if (allResults.length < totalResults || typeof rsp.data.results === 'undefined') {
                 recursiveGet((rsp.data.results[rsp.data.results.length - 1].id), totalResults);
             }else{
+                //Otherwise, it's done!
                 console.log('DONE!')
                 processFullResults();
             }
@@ -66,15 +75,18 @@ function recursiveGet(min_id, totalResults) {
     })
 }
 
+//Get the total number of results and the id of the first result
 function getNumResults() {
-    let url = `https://api.inaturalist.org/v1/observations?lat=40.78885994449482&lng=-74.1577148&radius=${radius}&order=asc&order_by=id&per_page=1`
+    let url = `https://api.inaturalist.org/v1/observations?lat=${lat}&lng=${lng}8&radius=${radius}&order=asc&order_by=id&per_page=1`
     axios.get(url).then((rsp) => {
         let total = rsp.data.total_results;
         console.log(total)
+        //Start off by looking at 1 less than the first id
         recursiveGet(+rsp.data.results[0].id - 1, total);
 
 
     })
 }
 
-getNumResults()
+//Start it all off
+getNumResults();
